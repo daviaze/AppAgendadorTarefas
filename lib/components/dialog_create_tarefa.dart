bf638f0e3553b23597aeae01e07f532b887ee900
daviaze <davisilcampos@hotmail.com>
2022-09-27 15:47:50 -0300

    Projeto finalizado

--- lib/components/dialog_create_tarefa.dart
+++ lib/components/dialog_create_tarefa.dart
@@ -1,48 +1,89 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class DialogCreateTarefa extends StatelessWidget {
   final Function criarTarefa;
-  const DialogCreateTarefa({Key? key, required this.criarTarefa}) : super(key: key);
+  const DialogCreateTarefa({Key? key, required this.criarTarefa})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     TextEditingController nomeTarefaController = TextEditingController();
     TextEditingController dificuldadeController = TextEditingController();
+    final _formKey = GlobalKey<FormState>();
+    var duration = const Duration(seconds: 5);
 
-
-    return SingleChildScrollView(//SingleChildScrollView faz com que quando o teclado é acionado, ele não exprema o container, podendo causar assim um overflow, ele irá deixar o container rolável quanto o teclado for acionado
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            controller: nomeTarefaController,
-            decoration: const InputDecoration(hintText: "Nome da tarefa"),
-          ),
-          TextFormField(
-            controller: dificuldadeController,
-            decoration: const InputDecoration(hintText: "Dificuldade"),
-            keyboardType: TextInputType.number,
-          ),
-          const SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () {
-              criarTarefa(nomeTarefaController.text, int.parse(dificuldadeController.text));
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-
-                Text("Criar"),
-              ],
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        //SingleChildScrollView faz com que quando o teclado é acionado, ele não exprema o container, podendo causar assim um overflow, ele irá deixar o container rolável quanto o teclado for acionado
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: nomeTarefaController,
+              decoration: const InputDecoration(hintText: "Nome da tarefa"),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Insira o nome da tarefa';
+                }
+                return null;
+              },
             ),
-            /*style: ButtonStyle(
-              padding: MaterialStatePropertyAll(
-                EdgeInsets.only(left: 90, right: 90),
+            TextFormField(
+              controller: dificuldadeController,
+              decoration: const InputDecoration(hintText: "Dificuldade"),
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Insira um valor para dificuldade';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  /*showDialog(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                        backgroundColor: Colors.transparent,
+                            content: SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.8,
+                              child: Lottie.asset(
+                                'assets/images/load_task.json',
+                                width: 60,
+                                height: 200,
+                                fit: BoxFit.scaleDown,
+                                repeat: true,
+                                reverse: false,
+                                animate: true,
+                              ),
+                            ),
+                          ));*/
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text("Tarefa criada!"),
+                    ),
+                  );
+                  criarTarefa(nomeTarefaController.text,
+                      int.parse(dificuldadeController.text));
+                  Navigator.of(context).pop( );
+                }
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text("Criar"),
+                ],
               ),
-            ),*/
-          )
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
